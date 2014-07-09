3032f2b3a470cabee408ba5c29e7b7ccf8115a89
oneski <davidesilverman@yahoo.com>
2014-07-09 18:49:29 -0400

    updates

--- hw2.py
+++ hw2.py
@@ -1,4 +1,13 @@
-def is_isbn(input):
+def is_isbn(ISBN):
+
+	"""TAKE ISBN RETURN TRUE IF VALID"""
+	checkDigit = calculate_check(ISBN)
+	if(checkDigit == 10):
+		checkDigit = "X"
+	if(checkDigit == ISBN[9]):
+		return True
+	else:
+		return False
 
 	"""TAKE ISBN RETURN TRUE IF VALID"""
 
@@ -9,17 +18,12 @@ def calculate_check(ISBN):
 	else:
 		return sum([int(a)*b for a,b in zip(list(ISBN[:9]),range(1,10))])%11
 
-
-calculate_check_test("123456789X")
-
-
-"""ZIP IS A FUNCTION THAT TAKES 2 LISTS, [1,2,3] and [a,b,c] RESULT IS TUPLE LIST[(1,a),(2,b),(3,c)]
-
-l = [(1,a),(2,b),(3,c)]
-
-'[(x,y) for x,y in l]'
+print calculate_check("123456789X")
+print is_isbn("123456789X")
 
 
-a*b for a,b in zip(a,b)
+#TO DO:
 
-"""
+#ERROR PREVENT IS_ISBN FOR 10 DIGIT String
+#ERROR PREVENT CALCULATE_CHECK FOR String
+#BUG FIX
