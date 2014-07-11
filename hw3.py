e451b57db9d4cef33e12c8982ef4db2df8fdaa65
oneski <davidesilverman@yahoo.com>
2014-07-10 20:31:27 -0400

    added tree

--- hw3.py
+++ hw3.py
@@ -1,2 +1,20 @@
 ###FILE FOR HW 3
 ###PARTNER = VIVIAN
+""" 							   <s>
+
+				<np>								<vp>
+		<dp><adjp><n>  | <pn>      			<tv><np> |  <iv>
+
+
+
+
+		<dp>			<adjp>														<n>
+	"the" | "a"		<adj>  |  <adj><adjp>		"dog" | "cat" | "man" | "university" | "father" | "mother" | "child" | "television" 
+
+
+						<pn>																		<adj>
+	"John" | "Jane" | "Sally" | "Spot" | "Fred" | "Elmo"			"big" | "fat" | "green" | "wonderful" | "faulty" | "subliminal" | "pretentious"
+
+					<tv>														<iv>
+		"hit" | "honored" | "kissed" | "helped" 				"died" | "collased" | "laughed" | "wept"
+"""
