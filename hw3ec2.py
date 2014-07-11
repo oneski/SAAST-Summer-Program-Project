336bc5374874f1ed143d92a9e18b4bf44ae8202c
oneski <davidesilverman@yahoo.com>
2014-07-11 09:42:26 -0400

    updated code

--- hw3ec2.py
+++ hw3ec2.py
@@ -30,7 +30,47 @@ def safe_permutations(listt,debugTest):
 		#print output
 	print debugTest
 	return output
-	return debugTest
+	return debugTests
 	
 
-print safe_permutations([1,2,3,4,5,6],debugTest)
+#print safe_permutations([1,2,3,4,5,6],debugTest)
+
+def remover(listt,index):
+	output = []
+	for item in listt:
+		print item
+		print listt[index]
+		if(listt[index] != item):
+			print item
+			output += [item]
+	return output
+
+def permutations_final(listt):
+	#enjoy
+	pass
+
+"""
+GOAL OF FINAL VERSION:
+
+[1,2,3]  = input
+
+output = []
+	
+ 	1				2			3
+ [1,2,3][0]  -> [2,3][0]	-> [3][0] -> None  therefore add combination to the list... and back track
+
+ 							<-cant iterate
+ 	1				3				2
+ [1,2,3][0]  -> [2,3][1]	->	[2][0] -> None therefore add combination to the list... and back track
+
+ 			<- cant iterate	<- cant iterate
+ 	2				1				3
+ [1,2,3][1]	 -> [1,3][0]	->	[3][0] -> None therefore add combination to the list... and back track
+
+.....
+
+ <-  cant iterate <- cant iterate <- cant iterate     NO1 can iterate therefore return output list
+
+return [[1,2,3],[1,3,2]...[3,2,1]]
+
+"""
