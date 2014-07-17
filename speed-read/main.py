b94e4a0d3da2111ff106fcf288dcb53bbb6fb6a5
oneski <davidesilverman@yahoo.com>
2014-07-17 16:54:16 -0400

    updaaaattteeess

--- speed-read/main.py
+++ speed-read/main.py
@@ -13,21 +13,23 @@ class WordGenerator:
 			self.words = temp
 		
 	def next_word(self):
-		if(self.lastAccess<len(self.words)):
-			temp = self.in_quotes
+		if not self.is_empty():
 			output = self.words[self.lastAccess]
-			if self.in_quotes == False and "\"" in self.words[self.lastAccess]:
-				temp = True
-				self.in_quotes = True
-			if self.in_quotes == True and "\"" in self.words[self.lastAccess]:
-				self.in_quotes = False
 			self.lastAccess += 1
-			return (output, temp)
+			if '"' in output:
+				if(self.in_quotes):
+					self.in_quotes = False
+					return (output, True)
+				else:
+					self.in_quotes = True
+					return (output, True)
+			else:
+				return (output, self.in_quotes)
 		else:
 			raise ValueError("End of File")
 
 	def is_empty(self):
-		return True if self.lastAccess >= len(self.words) else False
+		return self.lastAccess >= len(self.words)
 
 def main(args):
 	if len(args) == 6:
@@ -40,15 +42,15 @@ def main(args):
 		print "fuck off"
 
 def animate(generator, height=500,width=500,fontt=16,timeout=200):
-	def create_textt(canvas,fontr,textt,colorr="#FFF"):
-		canvas.create_text(height/2,width/2,text=textt,font=fontr,color=colorr)
+	def create_textt(canvas,fontr,textt,colorr="#000"):
+		canvas.create_text(height/2,width/2,text=textt,font=fontr,fill=colorr)
 
 	def sleep(obj,timee):
 		obj.update()
 		time.sleep(timee / 1000.0)
 		obj.update()
 
-	fonter = ("Helvetica", fontt)
+	fonter = ("Courier", fontt)
 	panal = DrawingPanel(height,width)
 	canvas = panal.canvas
 
@@ -57,18 +59,18 @@ def animate(generator, height=500,width=500,fontt=16,timeout=200):
 			canvas.create_rectangle(0, 0, height, width, fill = "white")
 			word = generator.next_word()
 			if(word[1]):
-				if(word[len(word[0])-1] == "." or word[len(word[0])-1] == "," or word[len(word[0])-1] == ":" or word[len(word[0])-1] == ";" or word[len(word[0])-1] == "!" or word[len(word[0])-1] == "?"):
-					create_textt(canvas,fonter,word,"#00b")
+				if(word[0][len(word[0])-1] == "." or word[0][len(word[0])-1] == "," or word[0][len(word[0])-1] == ":" or word[0][len(word[0])-1] == ";" or word[0][len(word[0])-1] == "!" or word[0][len(word[0])-1] == "?"):
+					create_textt(canvas,fonter,word[0],"#00b")
 					sleep(canvas,timeout*1.3)
 				else:
-					create_textt(canvas,fonter,word,"#00b")
+					create_textt(canvas,fonter,word[0],"#00b")
 					sleep(canvas,timeout)
 			else:
-				if(word[len(word[0])-1] == "." or word[len(word[0])-1] == "," or word[len(word[0])-1] == ":" or word[len(word[0])-1] == ";" or word[len(word[0])-1] == "!" or word[len(word[0])-1] == "?"):
-					create_textt(canvas,fonter,word)
+				if(word[0][len(word[0])-1] == "." or word[0][len(word[0])-1] == "," or word[0][len(word[0])-1] == ":" or word[0][len(word[0])-1] == ";" or word[0][len(word[0])-1] == "!" or word[0][len(word[0])-1] == "?"):
+					create_textt(canvas,fonter,word[0])
 					sleep(canvas,timeout*1.3)
 				else:
-					create_textt(canvas,fonter,word)
+					create_textt(canvas,fonter,word[0])
 					sleep(canvas,timeout)
 		except ValueError:
 			print "End of File"
