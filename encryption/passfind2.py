00041f22ba93c195eddc85db94c8526f2eb3762b
oneski <davidesilverman@yahoo.com>
2014-07-18 15:05:59 -0400

    updates

--- encryption/passfind2.py
+++ encryption/passfind2.py
@@ -1,21 +1,41 @@
-def main(stringg,wordList):
+def compute_inventories(words):
+	output = {}
+	for i in range(len(words)):
+		output[words] = LetterInv(word)
+	return output
+
+def prune_by_inventory(dic, li):
+	return {key : dic[key] for key in dic.keys() if is_in(dic[key], li)}
+
+def prune_by_length(dic, n):
+	return {key : dic[key] for key in dic.keys() if len(key) >= n}
+
+def prune_by_w(dic):
+	return {key : dic[key] for key in dic.keys() if key[0] == w}
+
+def notMain(stringg,wordList):
 	output = []
 	def iterator(stringgg,wordListt,listBuiltSoFar):
 		invent = create_inventory(stringg)
-		for key in WordList.getKeys():
-			if(is_in(WordList[key],invent)):
-				tempListBuiltSo = [x for x in listBuiltSoFar]
-				tempListBuiltSo += [str(key)]
-				if to_str(subtract(wordList[key],invent)) == "":
-					output += [tempListBuiltSo]
+		WordListt = prune_by_inventory(wordListt,invent)
+		if len(WordListt):
+			for key in WordList.keys():
+				if(is_in(WordList[key],invent)):
+					tempListBuiltSo = [x for x in listBuiltSoFar]
+					tempListBuiltSo += [str(key)]
+					if to_str(subtract(wordList[key],invent)) == "":
+						output += [tempListBuiltSo]
+					else:
+						iterator(to_str(subtract(wordList[key],invent)),wordListt,tempListBuiltSo)
 				else:
-					iterator(to_str(subtract(wordList[key],invent)),wordListt,tempListBuiltSo)
-			else:
-				pass
+					pass
+		else:
+			pass
 
 
-	iterator(stringg,wordList)
+	iterator(stringg,wordList,[])
 	return output
+
 
 
 
