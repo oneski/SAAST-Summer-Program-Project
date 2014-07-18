5b095f6f356d8d2c488ca22cd88ec8a356008fa5
oneski <davidesilverman@yahoo.com>
2014-07-18 14:47:35 -0400

    fixes

--- encryption/passfind2.py
+++ encryption/passfind2.py
@@ -1,19 +1,36 @@
 def main(stringg,wordList):
 	output = []
-	current = []
-	def iterator(stringgg,wordListt):
+	def iterator(stringgg,wordListt,listBuiltSoFar):
 		invent = create_inventory(stringg)
 		for key in WordList.getKeys():
 			if(is_in(WordList[key],invent)):
-				current += [str(key)]
+				tempListBuiltSo = [x for x in listBuiltSoFar]
+				tempListBuiltSo += [str(key)]
 				if to_str(subtract(wordList[key],invent)) == "":
-					output += [current]
-					current = []
+					output += [tempListBuiltSo]
 				else:
-					iterator(to_str(subtract(wordList[key],invent)),wordListt)
-	
+					iterator(to_str(subtract(wordList[key],invent)),wordListt,tempListBuiltSo)
+			else:
+				pass
+
+
 	iterator(stringg,wordList)
 	return output
-	
 
-			
+
+
+	"""def permutate(inputt):
+	output = []
+	def iterate(listBuiltSoFar, leftoverInput):
+		if len(leftoverInput) == 1:
+			listBuiltSoFar.append(leftoverInput[0])
+			output.append(listBuiltSoFar)
+		else:	
+			for i in range(len(leftoverInput)):
+				tempLeftover = [x for x in leftoverInput]
+				tempLeftover.pop(i)
+				tempListBuilt = [x for x in listBuiltSoFar]
+				tempListBuilt.append(leftoverInput[i])
+				iterate(tempListBuilt, tempLeftover)
+	iterate([], inputt)
+	return output"""
