982854275d2dea16893c27b13952a7a6f0aba6d3
jonpao523 <jonpao523@gmail.com>
2014-07-22 13:53:40 -0400

    asdf

--- pygame/microgames-tester/games/maths/game.py
+++ pygame/microgames-tester/games/maths/game.py
@@ -59,15 +59,28 @@ def _load_image(name, x, y):
 # TODO: put your Sprite classes here
 
 class rotatingNumber(Sprite):
-    def __init__(self):
+    def __init__(self, x):
         Sprite.__init__(self)
         #randomly choose # btw 1-10
-        imgpath = os.path.join("games","maths",str(str(number)+".png"))
+        self.num = 0
+        imgpath = os.path.join("games","maths",str(str(self.num)+".png"))
         self.image, self.rect = _load_image(imgpath,300,100)
+        self.rect.x, self.rect.y = int(x * locals.WIDTH), int(3.0 * locals.HEIGHT / 4)
         #bounding box
     def update(self):
         pass
-    
+
+class rotatingOperation(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        #randomly choose # btw 1-10
+        self.operation = "add"
+        imgpath = os.path.join("games","maths",self.operation + ".png"))
+        self.image, self.rect = _load_image(imgpath,300,100)
+        self.rect.x, self.rect.y = int(2.0 * locals.WIDTH / 3), s3.0 * locals.HEIGHT / 4
+        #bounding box
+    def update(self):
+        pass
 
 ##### MICROGAME CLASS #########################################################
 
@@ -77,11 +90,12 @@ class MATHS(Microgame):
     def __init__(self):
         Microgame.__init__(self)
         # TODO: Initialization code here
-        
+
         self.numb1 = rotatingNumber()
         self.numb2 = rotatingNumber()
-        self.opp = rotatingOperation()
-        self.sprites = Group(self.numb1,self.numb2,self.opp)
+        self.op = rotatingOperation()
+        self.sprites = Group(self.numb1,self.numb2,self.op)
+
     def start(self):
         # TODO: Startup code here
         pass
